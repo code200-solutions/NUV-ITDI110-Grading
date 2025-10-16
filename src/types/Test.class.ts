6249fccbc04d072376982244d229e47fe575ee36
Furtrand-Gael <gael@code200.solutions>
2025-10-16 17:16:07 +1100

    Update: all excercises for all sequences added

--- src/types/Test.class.ts
+++ src/types/Test.class.ts
@@ -21,8 +21,7 @@ export class Exercise {
 }
 
 //creating some exercises for the sequences 
-/*
-const repere2a: Exercise[] = [
+const reperes1: Exercise[] = [
   new Exercise(1, "Answer 1"),
   new Exercise(2, "Answer 2"),
   new Exercise(3, "Answer 3"),
@@ -32,4 +31,31 @@ const repere2a: Exercise[] = [
   new Exercise(7, "Answer 7"),
   new Exercise(8, "Answer 8"),
 ];
-*/
+
+const reperes2a: Exercise[] = [
+  new Exercise(1, "Answer 1"),
+  new Exercise(2, "Answer 2"),
+  new Exercise(3, "Answer 3"), 
+  new Exercise(4, "Answer 4"),
+  new Exercise(5, "Answer 5"),
+  new Exercise(6, "Answer 6"),
+  new Exercise(7, "Answer 7"),
+  new Exercise(8, "Answer 8"),
+  new Exercise(9, "Answer 9"),
+  new Exercise(10, "Answer 10"),
+  new Exercise(11, "Answer 11"),
+];
+
+const reperes3: Exercise[] = [
+  new Exercise(1, "Answer 1"),
+  new Exercise(2, "Answer 2"),
+  new Exercise(3, "Answer 3"),
+  new Exercise(4, "Answer 4"),
+  new Exercise(5, "Answer 5"),
+  new Exercise(6, "Answer 6"),
+  new Exercise(7, "Answer 7"),
+  new Exercise(8, "Answer 8"),
+  new Exercise(9, "Answer 9"),
+];
+
+
