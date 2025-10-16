e2485330befd6c0044e40f5b0407ad3e4e6ef1c3
Furtrand-Gael <gael@code200.solutions>
2025-10-16 16:53:20 +1100

    Update: contents of test classes identified and defined

--- src/types/Test.class.ts
+++ src/types/Test.class.ts
@@ -1,12 +1,35 @@
 // Define the content of the Test class
 export class Sequence {
   // ...
+  sequenceNum: number;
+  exercises: Exercise[];
+
+  constructor(sequenceNum: number, exercices: Exercise[]) {
+   this.sequenceNum = sequenceNum;
+   this.exercises = [];
+  }
 }
 
-export class Exercice {
-  // ...
+export class Exercise {
+ question: number;
+ answer: string;
+
+ constructor(question, answer ){
+  this.question = question;
+  this.answer = answer;
+ }
 }
 
-
-
-const repere2a = ....;
+//creating some exercises for the sequences 
+/*
+const repere2a: Exercise[] = [
+  new Exercise(1, "Answer 1"),
+  new Exercise(2, "Answer 2"),
+  new Exercise(3, "Answer 3"),
+  new Exercise(4, "Answer 4"),
+  new Exercise(5, "Answer 5"),
+  new Exercise(6, "Answer 6"),
+  new Exercise(7, "Answer 7"),
+  new Exercise(8, "Answer 8"),
+];
+*/
