02407c2c380c86867361f45e29777c8f1c1a1fb8
Furtrand-Gael <gael@code200.solutions>
2025-10-17 14:43:48 +1100

    Update: Changing exercise Id

--- src/types/Test.class.ts
+++ src/types/Test.class.ts
@@ -2,6 +2,15 @@
 export type ExerciseType = 'listening' | 'reading';
 
 // Define the content of the Test class
+export class Test {
+  testId: string;
+  sequences: Sequence[];
+
+  constructor(testId: string, sequences: Sequence[]) {
+    this.testId = testId;
+    this.sequences = sequences;
+  }
+}
 export class AnswerChoices{
   id: string;
   text?: string;
@@ -84,18 +93,19 @@ const answerChoices3: AnswerChoices[] = [
 
 //creating some exercises for the sequences 
 const reperes1: Exercise[] = [
-  new Exercise(1, 'listening', "Question 1", answerChoices1, "A"),
-  new Exercise(2, 'listening', "Question 2", answerChoices1, "B"),
-  new Exercise(3, 'listening', "Question 3", answerChoices1, "C"),
-  new Exercise(4, 'listening', "Question 4", answerChoices1, "D"),
-  new Exercise(5, 'listening', "Question 5", answerChoices1, "A"),
-  new Exercise(6, 'listening', "Question 6", answerChoices1, "B"),
-  new Exercise(7, 'listening', "Question 7", answerChoices1, "C"),
-  new Exercise(8, 'listening', "Question 8", answerChoices1, "D"),
+  new Exercise("rep1-seq1-ex1", 'listening', "Question 1", answerChoices1, "A"),
+  new Exercise("rep1-seq1-ex2", 'listening', "Question 2", answerChoices1, "B"),
+  new Exercise("rep1-seq1-ex3", 'listening', "Question 3", answerChoices1, "C"),
+  new Exercise("rep1-seq2-ex4", 'listening', "Question 4", answerChoices1, "D"),
+  new Exercise("rep1-seq2-ex5", 'listening', "Question 5", answerChoices1, "A"),
+  new Exercise("rep1-seq3-ex6", 'listening', "Question 6", answerChoices1, "B"),
+  new Exercise("rep1-seq3-ex7", 'listening', "Question 7", answerChoices1, "C"),
+  new Exercise("rep1-seq3-ex8", 'listening', "Question 8", answerChoices1, "D"),
 ];
 
+/*
 const reperes2a: Exercise[] = [
-  new Exercise(1, 'reading', "Question 1", answerChoices2, "A"),
+  new Exercise("rep2a-seq1-ex1", 'reading', "Question 1", answerChoices2, "A"),
   new Exercise(2, 'reading', "Question 2", answerChoices2, "B"),
   new Exercise(3, 'reading', "Question 3", answerChoices2, "C"),
   new Exercise(4, 'reading', "Question 4", answerChoices2, "D"),  
@@ -117,3 +127,4 @@ const reperes3: Exercise[] = [
   new Exercise(8, 'reading', "Question 8", answerChoices3, "D"),
   new Exercise(9, 'reading', "Question 9", answerChoices3, "A"),
 ];
+*/
