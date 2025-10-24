1697def1b12b8e800f1bd8d37d49c23a93992ab7
Furtrand-Gael <gael@code200.solutions>
2025-10-24 15:57:21 +1100

    Update: layout for the main tabs

---
+++ src/app/(tabs)/layout.tsx
@@ -0,0 +1,74 @@
+import { Redirect, Tabs } from 'expo-router';
+import React from 'react';
+import { Icon } from 'react-native-paper';
+
+export default function ProtectedTabLayout() {
+
+    type tabBarIconProps = {
+    focused: boolean;
+    color: string;
+    size: number;
+  };
+
+  return(
+    <Tabs
+        screenOptions={{
+          freezeOnBlur: true,
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen
+          name='home'
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ focused, color, size }: tabBarIconProps) => (
+              <Icon
+                source={focused ? 'home' : 'home-outline'}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='tests'
+          options={{
+            title: 'Tests',
+            tabBarIcon: ({ focused, color, size }: tabBarIconProps) => (
+              <Icon
+                source={focused ? 'account' : 'account-outline'}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='grades'
+          options={{
+            title: 'Grades',
+            tabBarIcon: ({ focused, color, size }: tabBarIconProps) => (
+              <Icon
+                source={focused ? 'bell' : 'bell-outline'}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='profile'
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ focused, color, size }: tabBarIconProps) => (
+              <Icon
+                source={focused ? 'clock' : 'clock-outline'}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+  );
+}
---
+++ src/app/(tabs)/testTabScreen.tsx
@@ -0,0 +1,8 @@
+import {
+  View,
+  StyleSheet,
+  Platform,
+  ViewStyle,
+  ScrollView,
+} from 'react-native';
+import { useEffect, useMemo, useState } from 'react';
