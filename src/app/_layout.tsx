4b307d42c24d63bed787f66766eff5b2c6f7d260
Furtrand-Gael <gael@code200.solutions>
2025-10-27 16:13:58 +1100

    Update: Status Bar color added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import '../global.css';
 import { Stack } from 'expo-router';
 import SafeAreaView from '../app/components/SafeAreaView';
+import { StatusBar } from 'react-native';
 
 export default function Layout() {
   return (
   <SafeAreaView>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="#c5c5c5ff" 
+      hidden={false} 
+    />
   <Stack
     screenOptions={{
         headerShown: false,
