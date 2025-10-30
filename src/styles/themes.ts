de38d1bbcfae6686bb54b94bd7937ece02b9b140
Furtrand-Gael <gael@code200.solutions>
2025-10-31 10:11:20 +1100

    Update: Adding dark color theme

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,4 +1,4 @@
-export const Theme = {
+export const LightTheme = {
   "colors": {
     "primary": "rgb(55, 60, 255)",
     "onPrimary": "rgb(255, 255, 255)",
@@ -42,3 +42,48 @@ export const Theme = {
     "backdrop": "rgba(48, 48, 56, 0.4)"
   }
 }; 
+
+const DarkColorTheme = {
+  "colors": {
+    "primary": "rgb(191, 193, 255)",
+    "onPrimary": "rgb(7, 0, 171)",
+    "primaryContainer": "rgb(14, 0, 237)",
+    "onPrimaryContainer": "rgb(225, 224, 255)",
+    "secondary": "rgb(206, 189, 255)",
+    "onSecondary": "rgb(56, 0, 149)",
+    "secondaryContainer": "rgb(81, 0, 207)",
+    "onSecondaryContainer": "rgb(232, 221, 255)",
+    "tertiary": "rgb(255, 185, 90)",
+    "onTertiary": "rgb(70, 42, 0)",
+    "tertiaryContainer": "rgb(100, 63, 0)",
+    "onTertiaryContainer": "rgb(255, 221, 182)",
+    "error": "rgb(255, 180, 171)",
+    "onError": "rgb(105, 0, 5)",
+    "errorContainer": "rgb(147, 0, 10)",
+    "onErrorContainer": "rgb(255, 180, 171)",
+    "background": "rgb(27, 27, 31)",
+    "onBackground": "rgb(229, 225, 230)",
+    "surface": "rgb(27, 27, 31)",
+    "onSurface": "rgb(229, 225, 230)",
+    "surfaceVariant": "rgb(70, 70, 79)",
+    "onSurfaceVariant": "rgb(199, 197, 208)",
+    "outline": "rgb(145, 143, 154)",
+    "outlineVariant": "rgb(70, 70, 79)",
+    "shadow": "rgb(0, 0, 0)",
+    "scrim": "rgb(0, 0, 0)",
+    "inverseSurface": "rgb(229, 225, 230)",
+    "inverseOnSurface": "rgb(49, 48, 52)",
+    "inversePrimary": "rgb(55, 60, 255)",
+    "elevation": {
+      "level0": "transparent",
+      "level1": "rgb(35, 35, 42)",
+      "level2": "rgb(40, 40, 49)",
+      "level3": "rgb(45, 45, 56)",
+      "level4": "rgb(47, 47, 58)",
+      "level5": "rgb(50, 50, 62)"
+    },
+    "surfaceDisabled": "rgba(229, 225, 230, 0.12)",
+    "onSurfaceDisabled": "rgba(229, 225, 230, 0.38)",
+    "backdrop": "rgba(48, 48, 56, 0.4)"
+  }
+}
