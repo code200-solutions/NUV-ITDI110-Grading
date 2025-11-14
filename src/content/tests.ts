7e0ff8df24fde9004cae6036c29a8d489b58425e
Furtrand-Gael <gael@code200.solutions>
2025-11-14 11:38:41 +1100

    Update: changing instances for test 2 and 3

--- src/content/tests.ts
+++ src/content/tests.ts
@@ -23,35 +23,51 @@ const imgRep1Seq1Ex1Img19 = require('@/assets/images/reperes1/rep1-seq1-ex1-img1
 const imgRep1Seq1Ex1Img20 = require('@/assets/images/reperes1/rep1-seq1-ex1-img20.png');
 
 //answer choices for exercise 1 or Reperes - 1
-export const answerChoices1: AnswerChoice[] = [
+export const test1AnswerChoices1: AnswerChoice[] = [
   new ImageAnswerChoice('A', imgRep1Seq1Ex1Img1),
   new ImageAnswerChoice('B', imgRep1Seq1Ex1Img2),
   new ImageAnswerChoice('C', imgRep1Seq1Ex1Img3),
   new ImageAnswerChoice('D', imgRep1Seq1Ex1Img4),
 ];
 
-export const answerChoices2: AnswerChoice[] = [
+export const test1AnswerChoices2: AnswerChoice[] = [
   new ImageAnswerChoice('A', imgRep1Seq1Ex1Img5),
   new ImageAnswerChoice('B', imgRep1Seq1Ex1Img6),
   new ImageAnswerChoice('C', imgRep1Seq1Ex1Img7),
   new ImageAnswerChoice('D', imgRep1Seq1Ex1Img8),
 ]; 
 
-export const answerChoices3: AnswerChoice[] = [
+export const test1AnswerChoices3: AnswerChoice[] = [
   new ImageAnswerChoice('A', imgRep1Seq1Ex1Img9),
   new ImageAnswerChoice('B', imgRep1Seq1Ex1Img10),
   new ImageAnswerChoice('C', imgRep1Seq1Ex1Img11),
   new ImageAnswerChoice('D', imgRep1Seq1Ex1Img12),
 ];
 
-export const answerChoices4: AnswerChoice[] = [
+export const test1AnswerChoices4: AnswerChoice[] = [
   new ImageAnswerChoice('A', imgRep1Seq1Ex1Img13),
   new ImageAnswerChoice('B', imgRep1Seq1Ex1Img14),
   new ImageAnswerChoice('C', imgRep1Seq1Ex1Img15),
   new ImageAnswerChoice('D', imgRep1Seq1Ex1Img16),
 ];
 
-export const answerChoices5: AnswerChoice[] = [
+export const test1AnswerChoices5: AnswerChoice[] = [
+  new ImageAnswerChoice('A', imgRep1Seq1Ex1Img17),
+  new ImageAnswerChoice('B', imgRep1Seq1Ex1Img18),
+  new ImageAnswerChoice('C', imgRep1Seq1Ex1Img19),
+  new ImageAnswerChoice('D', imgRep1Seq1Ex1Img20),
+];
+
+//Answer choices for reperes 2a
+export const test2AnswerChoices1: AnswerChoice[] = [
+  new TextAnswerChoice('A', "Question 1"),
+  new TextAnswerChoice('B', "Question 1"),
+  new TextAnswerChoice('C', "Question 1"),
+  new TextAnswerChoice('D', "Question 1"),
+];
+
+//Answer choices for reperes 3
+export const test3AnswerChoices1: AnswerChoice[] = [
   new ImageAnswerChoice('A', imgRep1Seq1Ex1Img17),
   new ImageAnswerChoice('B', imgRep1Seq1Ex1Img18),
   new ImageAnswerChoice('C', imgRep1Seq1Ex1Img19),
@@ -60,37 +76,37 @@ export const answerChoices5: AnswerChoice[] = [
 
 //creating some exercises for the sequences 
 export const reperes1: Exercise[] = [
-  new Exercise("rep1-seq1-ex1", 'listening', "Question 1", answerChoices1, "A"),
-  new Exercise("rep1-seq1-ex2", 'listening', "Question 2", answerChoices2, "B"),
-  new Exercise("rep1-seq1-ex3", 'listening', "Question 3", answerChoices3, "C"),
-  new Exercise("rep1-seq2-ex4", 'listening', "Question 4", answerChoices4, "D"),
-  new Exercise("rep1-seq2-ex5", 'listening', "Question 5", answerChoices5, "A"),
+  new Exercise("rep1-seq1-ex1", 'listening', "Question 1", test1AnswerChoices1, "A"),
+  new Exercise("rep1-seq1-ex2", 'listening', "Question 2", test1AnswerChoices1, "B"),
+  new Exercise("rep1-seq1-ex3", 'listening', "Question 3", test1AnswerChoices1, "C"),
+  new Exercise("rep1-seq2-ex4", 'listening', "Question 4", test1AnswerChoices1, "D"),
+  new Exercise("rep1-seq2-ex5", 'listening', "Question 5", test1AnswerChoices1, "A"),
 ];
 
 export const reperes2a: Exercise[] = [
-  new Exercise("rep2a-seq1-ex1", 'reading', "Question 1", answerChoices2, "A"),
-  new Exercise("rep2a-seq1-ex2", 'reading', "Question 2", answerChoices2, "B"),
-  new Exercise("rep2a-seq1-ex3", 'reading', "Question 3", answerChoices2, "C"),
-  new Exercise("rep2a-seq1-ex4", 'reading', "Question 4", answerChoices2, "D"),  
-  new Exercise("rep2a-seq2-ex5", 'reading', "Question 5", answerChoices2, "A"),
-  new Exercise("rep2a-seq2-ex6", 'reading', "Question 6", answerChoices2, "B"),
-  new Exercise("rep2a-seq2-ex7", 'reading', "Question 7", answerChoices2, "C"),
-  new Exercise("rep2a-seq3-ex8", 'reading', "Question 8", answerChoices2, "D"),
-  new Exercise("rep2a-seq3-ex9", 'reading', "Question 9", answerChoices2, "A"),
-  new Exercise("rep2a-seq3-ex10", 'reading', "Question 10", answerChoices2, "B"),
-  new Exercise("rep2a-seq3-ex11", 'reading', "Question 11", answerChoices2, "C"),
+  new Exercise("rep2a-seq1-ex1", 'reading', "Question 1", test2AnswerChoices1, "A"),
+  new Exercise("rep2a-seq1-ex2", 'reading', "Question 2", test2AnswerChoices1, "B"),
+  new Exercise("rep2a-seq1-ex3", 'reading', "Question 3", test2AnswerChoices1, "C"),
+  new Exercise("rep2a-seq1-ex4", 'reading', "Question 4", test2AnswerChoices1, "D"),  
+  new Exercise("rep2a-seq2-ex5", 'reading', "Question 5", test2AnswerChoices1, "A"),
+  new Exercise("rep2a-seq2-ex6", 'reading', "Question 6", test2AnswerChoices1, "B"),
+  new Exercise("rep2a-seq2-ex7", 'reading', "Question 7", test2AnswerChoices1, "C"),
+  new Exercise("rep2a-seq3-ex8", 'reading', "Question 8", test2AnswerChoices1, "D"),
+  new Exercise("rep2a-seq3-ex9", 'reading', "Question 9", test2AnswerChoices1, "A"),
+  new Exercise("rep2a-seq3-ex10", 'reading', "Question 10", test2AnswerChoices1, "B"),
+  new Exercise("rep2a-seq3-ex11", 'reading', "Question 11", test2AnswerChoices1, "C"),
 ];
 
 export const reperes3: Exercise[] = [
-  new Exercise("rep3-seq1-ex1", 'reading', "Question 1", answerChoices3, "A"),
-  new Exercise("rep3-seq1-ex2", 'reading', "Question 2", answerChoices3, "B"),
-  new Exercise("rep3-seq1-ex3", 'reading', "Question 3", answerChoices3, "C"),
-  new Exercise("rep3-seq1-ex4", 'reading', "Question 4", answerChoices3, "D"),
-  new Exercise("rep3-seq2-ex5", 'reading', "Question 5", answerChoices3, "A"),
-  new Exercise("rep3-seq2-ex6", 'reading', "Question 6", answerChoices3, "B"),
-  new Exercise("rep3-seq2-ex7", 'reading', "Question 7", answerChoices3, "C"),
-  new Exercise("rep3-seq3-ex8", 'reading', "Question 8", answerChoices3, "D"),
-  new Exercise("rep3-seq3-ex9", 'reading', "Question 9", answerChoices3, "A"),
+  new Exercise("rep3-seq1-ex1", 'reading', "Question 1", test3AnswerChoices1, "A"),
+  new Exercise("rep3-seq1-ex2", 'reading', "Question 2", test3AnswerChoices1, "B"),
+  new Exercise("rep3-seq1-ex3", 'reading', "Question 3", test3AnswerChoices1, "C"),
+  new Exercise("rep3-seq1-ex4", 'reading', "Question 4", test3AnswerChoices1, "D"),
+  new Exercise("rep3-seq2-ex5", 'reading', "Question 5", test3AnswerChoices1, "A"),
+  new Exercise("rep3-seq2-ex6", 'reading', "Question 6", test3AnswerChoices1, "B"),
+  new Exercise("rep3-seq2-ex7", 'reading', "Question 7", test3AnswerChoices1, "C"),
+  new Exercise("rep3-seq3-ex8", 'reading', "Question 8", test3AnswerChoices1, "D"),
+  new Exercise("rep3-seq3-ex9", 'reading', "Question 9", test3AnswerChoices1, "A"),
 ];
 
 export const Tests: Test[] = [
